c1160237576ea8f631f375ba18b90455a7324113
Johnny <jonathanbc2012@gmail.com>
2024-07-05 17:08:26 +0200

    anotations added to Cliente class

--- proyectos_backend/microservicio_clientes_01/src/main/java/com/market/init/microservicio_clientes_01/model/Cliente.java
+++ proyectos_backend/microservicio_clientes_01/src/main/java/com/market/init/microservicio_clientes_01/model/Cliente.java
@@ -1,5 +1,8 @@
 package com.market.init.microservicio_clientes_01.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +10,10 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name="clientes")
 public class Cliente {
+    @Id
     private String usuario;
     private String password;
     private String nombre;
