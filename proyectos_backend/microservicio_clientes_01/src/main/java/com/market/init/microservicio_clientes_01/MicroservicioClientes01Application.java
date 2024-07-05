b5838308531bf3a43141fb27722a532d9fe105f7
Johnny <jonathanbc2012@gmail.com>
2024-07-05 16:41:48 +0200

    microservicios_clientes_01 project created

---
+++ proyectos_backend/microservicio_clientes_01/src/main/java/com/market/init/microservicio_clientes_01/MicroservicioClientes01Application.java
@@ -0,0 +1,13 @@
+package com.market.init.microservicio_clientes_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioClientes01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioClientes01Application.class, args);
+	}
+
+}
---
+++ proyectos_backend/microservicio_clientes_01/src/test/java/com/market/init/microservicio_clientes_01/MicroservicioClientes01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.init.microservicio_clientes_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioClientes01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
