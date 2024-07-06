54d08c026666a9a13108ec233fee34f346350816
Johnny <jonathanbc2012@gmail.com>
2024-07-06 18:59:06 +0200

    backend microservicio clientes finished and tested with postman

--- proyectos_backend/microservicio_clientes_01/src/main/java/com/market/init/microservicio_clientes_01/MicroservicioClientes01Application.java
+++ proyectos_backend/microservicio_clientes_01/src/main/java/com/market/init/microservicio_clientes_01/MicroservicioClientes01Application.java
@@ -2,12 +2,13 @@ package com.market.init.microservicio_clientes_01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+/* import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories; */
 
-@EntityScan(basePackages = "com.market.init.microservicio_clientes_01.model")
+/* @EntityScan(basePackages = "com.market.init.microservicio_clientes_01.model")
 @EnableJpaRepositories(basePackages = "com.market.init.microservicio_clientes_01.persistence")
-@SpringBootApplication(scanBasePackages = {"com.market.init.microservicio_clientes_01.service","com.market.init.microservicio_clientes_01.controller"})
+@SpringBootApplication(scanBasePackages = {"com.market.init.microservicio_clientes_01.service","com.market.init.microservicio_clientes_01.controller"}) */
+@SpringBootApplication
 public class MicroservicioClientes01Application {
 
 	public static void main(String[] args) {
