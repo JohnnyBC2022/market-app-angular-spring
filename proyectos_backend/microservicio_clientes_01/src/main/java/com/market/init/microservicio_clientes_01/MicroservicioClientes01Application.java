e1ee0347be4833d919a74947f96fe4a1a53fbc6e
Johnny <jonathanbc2012@gmail.com>
2024-07-06 12:55:22 +0200

    microservicioClientes01Application.java and application.properties modified

--- proyectos_backend/microservicio_clientes_01/src/main/java/com/market/init/microservicio_clientes_01/MicroservicioClientes01Application.java
+++ proyectos_backend/microservicio_clientes_01/src/main/java/com/market/init/microservicio_clientes_01/MicroservicioClientes01Application.java
@@ -2,8 +2,12 @@ package com.market.init.microservicio_clientes_01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EntityScan(basePackages = "com.market.init.microservicio_clientes_01.model")
+@EnableJpaRepositories(basePackages = "com.market.init.microservicio_clientes_01.persistence")
+@SpringBootApplication(scanBasePackages = {"com.market.init.microservicio_clientes_01.service","com.market.init.microservicio_clientes_01.controller"})
 public class MicroservicioClientes01Application {
 
 	public static void main(String[] args) {
