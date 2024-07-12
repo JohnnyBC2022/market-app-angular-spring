38dfc9b31fe341f132d0603f6af80be72ae616ae
Johnny <jonathanbc2012@gmail.com>
2024-07-12 14:19:36 +0200

    login controller created

---
+++ proyectos_frontend/marketclient/src/app/controller/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Cliente } from '../../model/Cliente';
+import { LoginService } from './../../service/login.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  usuario:string;
+  password:string;
+  client: Cliente;
+  constructor(private loginService:LoginService){
+
+  }
+
+  login(){
+    this.loginService.login(this.usuario,this.password).subscribe(data=>{
+      this.client=data;
+      if(this.client!=null){ //usuario válido
+        alert("Usuario autentificado");
+      }else{
+        alert("Usuario no autentificado")
+      }
+    });
+  }
+}
